2195b1aebbbe776adf9d965b35220f9a662cae63
animotime-v3 <madkim279@gmail.com>
2023-09-29 22:22:17 +0100

    *

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,5 +1,7 @@
 module.exports = ({ env }) => ({
   proxy: true,
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   url: env("https://dental-braces.onrender.com"), // Sets the public URL of the application.
   app: {
     keys: ["12333dlfshfhf", "fhizfhqhmfhmh"],
