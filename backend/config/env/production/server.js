c14a806b47a294742b2722bab429eb20671110bf
animotime-v3 <madkim279@gmail.com>
2023-10-05 20:28:48 +0100

    add yml file

--- backend/config/env/production/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  proxy: true,
-  host: "0.0.0.0",
-  port: 1337,
-  url: "https://dental-braces.onrender.com", // Sets the public URL of the application.
-  app: {
-    keys: ["12333dlfshfhf", "fhizfhqhmfhmh"],
-  },
-});
